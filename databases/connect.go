ace37b2f3b7d75d4579d35ff2a00bf4d6d30d82c
Hoon <kimhoon0316@gmail.com>
2020-11-10 16:42:11 +0900

    user email unique add,

--- databases/connect.go
+++ databases/connect.go
@@ -13,12 +13,12 @@ import (
 // ConnectDB connect db
 func ConnectDB() {
 	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_HOST"), config.Config("DB_PORT"), config.Config("DB_NAME"))
-	fmt.Println(dns)
-	DB, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
 		panic("failed to connect database")
 	}
+	DB = db
 
 	fmt.Println("Connect Database")
 	DB.AutoMigrate(&models.User{})
--- models/user.go
+++ models/user.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 // User struct
 type User struct {
 	gorm.Model
-	Email    string `gorm:"unique_index;not null" json:"email"`
+	Email    string `gorm:"unique;unique_index;not null" json:"email"`
 	Password string `gorm:"not null" json:"password"`
 	Name     string `json:"name"`
 }
